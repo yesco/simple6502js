931934613fdce6c72593ac331425a907f73c4c94
Jonas S Karlsson <jsk@yesco.org>
2021-05-31 16:41:40 +0700

    CMP etc defined

--- 6502-style.c
+++ 6502-style.c
@@ -3,47 +3,71 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define MERGE_(a,b)  a##b
-#define LABEL_(a) MERGE_(unique_name_, a)
-#define UNIQUE_NAME LABEL_(__COUNTER__)
-
 typedef unsigned char byte;
 typedef unsigned short word;
 
-// doesn't fit inside 2 bytes (32 bit pointers)
+// we're not really simulating very accurately
+// the stack is just a figment of our imagination
+//
+// TODO: this won't work with return address
+// manipulations... Also, JMPI (indirect)  is
+// also not going to work.
 void* stack[128] = {0};
 
 #define push stack[((s-=2)+2)/2] = (void*)
 #define pop_ stack[(s+=2)/2]
 #define pop ((byte) pop_)
 
+// lot this will take two slots
+// 
 #define PHA push a
 #define PLA a = pop
 
 // TODO: p is not set correctly yet
 //#define PHP push p
-//#define PLP p = pop()
+//#define PLP p = pop
 
+// sneaky stuff!
+// generates unique label name
+// - https://stackoverflow.com/questions/1132751/how-can-i-generate-unique-values-in-the-c-preprocessor
+#define MERGE_(a,b)  a##b
+#define LABEL_(a) MERGE_(unique_name_, a)
+#define UNIQUE_NAME LABEL_(__COUNTER__)
 
-#define JSR_(L, U) { push &&U; goto L; U: (void)0; }
-#define JSR(L) JSR_(L, UNIQUE_NAME)
+#ifndef SKIP
+  #define jsr_(L, U) { push &&U; goto L; U: (void)0; }
+  #define JSR(L) jsr_(L, UNIQUE_NAME)
+#endif
+
+#ifdef SKIP
+  #define jsr_(L, U) { U: push &&U + 4; goto L;}
+  #define JSR(L) jsr_(L, UNIQUE_NAME)
+#endif
+
 #define RTS { goto *pop_; }
 #define JMP goto
 
 #define BRK { goto brk; }
 
-//#define LDA _ = a = (_z=-1, 0) +
-//#define LDX _ = x = (_z=-1, 0) +
-//#define LDY _ = y = (_z=-1, 0) +
+// branches
+#define BNE if (!z) JMP
+#define BEQ if (z) JMP
+#define BCC if (!c) JMP
+#define BCS if (c) JMP
+#define BVC if (!v) JMP
+#define BVS if (v) JMP
+#define BPL if (!n) JMP
+#define BMI if (n) JMP
 
-#define LDA *(after(&a, 0, __LINE__)) = __ =
-#define STA *(after(&a, 1, __LINE__)) = __ =
+// load and store, inc/dex, transfer
+#define LDA *(after(&a, 1, __LINE__, 0)) = __ =
+#define STA *(after(&a, 2, __LINE__, 0)) = __ =
 
-#define LDX *(after(&x, 0, __LINE__)) = __ =
-#define STX *(after(&x, 1, __LINE__)) = __ =
+#define LDX *(after(&x, 1, __LINE__, 0)) = __ =
+#define STX *(after(&x, 2, __LINE__, 0)) = __ =
 
-#define LDY *(after(&y, 0, __LINE__)) = __ =
-#define STY *(after(&y, 1, __LINE__)) = __ =
+#define LDY *(after(&y, 1, __LINE__, 0)) = __ =
+#define STY *(after(&y, 2, __LINE__, 0)) = __ =
 
 #define TAX LDX a
 #define TXA LDA x
@@ -60,13 +84,17 @@ void* stack[128] = {0};
 #define INY LDY ((y+1) & 0xff)
 #define DEY LDY ((y-1) & 0xff)
 
+#define CMP *(after(&_, 3, __LINE__, a)) = __ =
+#define CPX *(after(&_, 3, __LINE__, x)) = __ =
+#define CPY *(after(&_, 3, __LINE__, y)) = __ =
 
-
+// addressing modes
+// (mark address as "authentic")
 #define MEM_MODE (0x30de)
 #define MEM(A) ((A) | 0x30de0000)
 
-#define abs(A) MEM(A)
 #define imm(V) ((V) & 0xff)
+#define abs(A) MEM(A)
 
 #define zp(A) MEM((A) & 0xff)
 #define zpx(A) zp((A)+x)
@@ -77,16 +105,16 @@ void* stack[128] = {0};
 #define zpxi(A) MEM(word( (A)+x) )
 #define zpiy(A) MEM(word( A )+y)
 
-byte a = 0, x = 0, y = 0, s = 0, mem[65536] = {0};
-byte z = 0, n = 0, v = 0, c = 0; // flags
+// registers
+byte a = 0, x = 0, y = 0, s = 0;
+byte mem[65536] = {0};
+// flags
+byte z = 0, n = 0, v = 0, c = 0;
 
-int _z = -1, _n = -1, _v = 0, _c = 0,  _, __, __A; // to update flags
+// temporary storage (for macros)
+int  _, __, __A, __skip;
 
-void UpdateFlags() {
-  // TODO: other flags?
-}
-
-int* after(byte *r, int ST, int line) {
+int* after(byte *r, int rwc, int line, byte cmp) {
   static int _;
 
   if ((v >> 16) == MEM_MODE) {
@@ -99,13 +127,19 @@ int* after(byte *r, int ST, int line) {
   int a = __ & 0xffff;
   byte v;
 
-  if (!ST) { // LD? - READ
+  if (rwc==1 || rwc==3) { // LD? - READ
     if (0 <= a && a <= 0xff) { // immediate
       v = *r = a & 0xff;
     } else {
       v = *r = mem[a];
     }
-  } else { // ST? - WRITE
+
+    if (rwc==3) { // compare, set C
+      c = !!(cmp >= v);
+      v = cmp - v;
+    }
+
+  } else if (rwc==2) { // ST? - WRITE
     if (0 <= a && a <= 0xff) {
       // no immediate mode
       fprintf(stderr, "\n\n%% Line %d: STA/STX/STY doesn't have immediate mode, please use 'STA/LDA <MODE>(0x501) for value: %x (%d)\n", line, a, a);
@@ -123,15 +157,28 @@ int* after(byte *r, int ST, int line) {
   return &_;
 }
 
-
-#define BNE if (UpdateFlags(), !z) JMP
-#define BEQ if (UpdateFlags(), z) JMP
-
 int main()
 {
   assert(sizeof(byte)==1);
   assert(sizeof(word)==2);
   assert(sizeof(int)==4);
+
+  // admin stuff
+  
+  if (1) {
+    void* after = (void*) &&unique_after;
+  unique_before:
+    // Assuming there is constant difference!
+    goto unique_go;
+  unique_after:
+    (void)0;
+
+  unique_go:
+
+    __skip = (int)&&unique_after - (int)&&unique_before;
+    //printf("SKIP=%d\n", __skip);
+  }
+  // CPU 65002 starts here
 
 
 reset:
@@ -139,13 +186,11 @@ reset:
 
 start:
 
-
-  LDA 0xff;
-
   goto printfibs;
 
 testbne:
-  LDA 1;
+  LDA 33;
+  CMP 33;
   BNE not;
   printf("z=%d\n", z);
   printf("equal!\n");
@@ -177,7 +222,7 @@ fib: // in: a, out: a=fib
 
   a--;
 
-  push a;
+  PHA;
   JSR(fib);
   TAX;
   PLA; //   PLA;
@@ -185,7 +230,7 @@ fib: // in: a, out: a=fib
   a--;
 
   push x;
-  push a;
+  PHA;
     
   JSR(fib);
   TAY;
