6fbbe9d37e1cb2b1d23209da2f84f555337daa45
Jonas S Karlsson <jsk@yesco.org>
2021-06-02 08:47:23 +0700

    adc (macros) for a Word CPU

--- wcpu.js
+++ wcpu.js
@@ -10,17 +10,41 @@
 //   fixed reg address (a/zp)
 // - Address can be a/zp:
 //   code generated is adopted.
-//
+
+// - x and y are preserved
+// - a is almost always trashed
+
+// (3)
 // - n2m
 // - n2x
 // - n2y
 
+// (5)
 // -   m2m
 // - m2x  x2m
 // - m2y  y2m
 
+// (3)
 // - x2y  y2x
 // -   x0y
+
+// (6)
+// - minc mdec (m2inc m2dec)
+// - xinc xdec (x2inc m2dec)
+// - yinc ydec (y2inc m2dec)
+
+// (10+10=20) ADD SUB
+// - mADDn mADDm mADDx mADDy
+// - xADDn xADDm xADDy
+// - yADDn yADDm yADDx
+
+// (10)
+// - mMULn mMULm mMULx mMULy
+// - xMULn xMULn xMULy
+// - yMULn yMULn yMULx
+
+// (10)
+// - ?div?
 
 function n2m(z, n) { // b8+2
   LDAN(n & 0xff);   sta(z);
@@ -66,10 +90,159 @@ function x0y() {
   bswapxy(1);
 }
 
-// - wyTXx(a)
-// - wxySW() - swap
+function minc(a) {
+  let l = gensym();
 
+  inc(a);
+  BNE(l);
+  inc(a+1);
+L(l);  
+}
+function xinc() { // b6 c9+5
+  let l = gensym();
 
+  INCZX(0);
+  BNE(l);
+  INCZX(1);
+L(l);  
+}
+function yinc() {
+  if (1) { // b8 c11+5
+    TXA();
+    xinc();
+    TAX();
+  } else { // b17 c22
+    LDAAY(0);
+    CLC();
+    ADCN(1);
+    STAAY(0);
+
+    LDAAY(1);
+    ADCN(0);
+    STAAY(1);
+  }
+}
+
+function mdec(a) {
+  let l = gensym();
+
+  lda(a);
+  BNE(l);
+  dec(a+1);
+L(l);
+  dec(a);
+}
+
+function xdec() { // b6 c9+5
+  let l = gensym();
+
+  LDAZX(0)
+  BNE(l);
+  DECZX(1);
+L(l);  
+  DECZX(0);
+}
+function ydec() {
+  if (1) { // b8 c11+5
+    TXA();
+    xdec();
+    TAX();
+  } else { // b17 c22
+    LDAAY(0);
+    SEC();
+    SBCN(1);
+    STAAY(0);
+
+    LDAAY(1);
+    SBCN(0);
+    STAAY(1);
+  }
+}
+
+function mADDn(a, n) {
+  LDAA(a);
+  CLC();
+  SBCN(n & 0xff);
+  STAA(a);
+
+  LDAA(a+1);
+  ADCN(n >> 8);
+  STAA(a+1);
+}
+function xADDn(n) {
+  LDAZX(0);
+  CLC();
+  ADCN(n & 0xff);
+  STAZX(0);
+
+  LDAZX(1);
+  ADCN(n >> 8);
+  STAZX(1);
+}
+function yADDn(n) {
+  LDAAY(0);
+  CLC();
+  ADCN(n & 0xff);
+  STAAY(0);
+
+  LDAAY(1);
+  ADCN(n >> 8);
+  STAAY(1);
+}
+
+function mADDm(z, a) {
+  lda(z);
+  CLC();
+  adc(a);
+  sta(z);
+  
+  lda(z+1);
+  adc(a+1);
+  sta(z+1);
+}
+function xADDm(a) {
+  LDAXZ(0);
+  CLC();
+  adc(a);
+  STAXZ(0);
+  
+  LDAXZ(1);
+  adc(a+1);
+  STAXZ(1);
+}
+function yADDm(a) {
+  LDAAY(0);
+  CLC();
+  adc(a);
+  STAAY(0);
+  
+  LDAAY(1);
+  adc(a+1);
+  STAAY(1);
+}
+
+function mADDx(a) {
+  lda(a);
+  CLC();
+  ADCZX(0);
+  sta(a);
+  
+  lda(a+1);
+  ADCZX(1);
+  sta(a+1);
+}
+function mADDy(a) {
+  lda(a);
+  CLC();
+  ADCAY(0);
+  sta(a);
+  
+  lda(a+1);
+  ADCAY(1);
+  sta(a+1);
+}
+
+////////////////////////////////
 
 function ni() {
   throw "%% Not implemented!\n");
@@ -102,5 +275,34 @@ function bswapxy(o) {
   EORZX(o); STAZX(o);
 }
 
+function inc(a) {
+  if (a < 256) {
+    INCZ(a);
+  } else {
+    INCA(a);
+  }
+}
 
+function cmp(a) {
+  if (a < 256) {
+    CMPZ(a);
+  } else {
+    CMPA(a);
+  }
+}
 
+function dec(a) {
+  if (a < 256) {
+    DECZ(a);
+  } else {
+    DECA(a);
+  }
+}
+
+function adc(a) {
+  if (a < 256) {
+    ADCZ(a);
+  } else {
+    ADCA(a);
+  }
+}
