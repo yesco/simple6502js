6e41d9df48c6a95c04b24c58e65a3c5a3710cbec
Jonas S Karlsson <jsk@yesco.org>
2024-08-21 00:53:33 +0700

    65lisp: cons is 'negative'

--- 65lisp/65lisp.c
+++ 65lisp/65lisp.c
@@ -7,47 +7,99 @@
 
 //#include <conio.h>
 
-// Lisp Datatypes
+
+// ---------------- Lisp Datatypes
+
+// -- Cons
+
 typedef int L;
-const int nil= 0;
+const int nil= 0; // hmmm
+int quote= 1; // hmmm, lol
 
 // can't be bigger than 32K
 // should be dynamic, allocate page by page?
-#define MAXCELL 32*1024/2
+#define MAXCELL 31*1024/2
 int ncell= 0;
 L cell[MAXCELL]= {0};
+
+L prin1(L); // forward TODO: remove
 
 L cons(L a, L d) {
   cell[ncell++]= a;
   cell[ncell++]= d;
-  return -(ncell-2);
+  return ncell-1;
 }
 
+// TODO: make macro
+#define consp(c) ((c)&1)
+
 L car(L c) {
-  return -(cell[c]);
+  return consp(c)? cell[c-1]: nil;
 }
 
 L cdr(L c) {
-  return -(cell[c+1]);
+  return consp(c)? cell[c]: nil;
 }
 
+
+
+// --- Atoms
+
+#define MAXSYMLEN 32
 L syms= 0;
 
-L print(L); // forward
-
-void* atom(char* s) {
-  char* p;
-  // TODO: global variable?
-  p= strdup(s);
-  //p= malloc(n+2);
-  //strcpy(p, s);
-  syms= cons(p, syms);
-  printf("\nSYMBOL: %04x '%s'\n", p, p);
-  print(syms);
-  return p;
+char isatomchar(char c) {
+  return (char)(int)!strchr(" \t\n\r`'\"\\()[]{}", c);
 }
 
-// IO
+L print(L); // forward TODO: remove
+
+L atom(char* s) {
+  char* p;
+  L r;
+  if (0==strcmp(s, "nil")) return nil;
+  p= strdup(s);
+  r= (int)p;
+  //p= malloc(n+2);
+  //strcpy(p, s);
+  syms= cons(r, syms);
+  printf("\nSYMBOL: %04x '%s'\n", p, p);
+  //print(syms);
+  return r;
+}
+
+L atomp(L x) {
+  // TODO: lol
+  return x&1;
+}
+
+
+// --- Strings
+
+L stringp(L x) {
+  return x?0:0;
+}
+
+
+// --- Numbers
+
+L numberp(L x) {
+  return (!(x&1));
+}
+
+int num(L x) {
+  if (!numberp(x)) return 0; // "safe"
+  return x/2-1;
+}
+
+L mknum(int n) {
+  L r;
+  // TODO: negatives?
+  assert(n >= 0);
+  return (n+1)*2;
+}
+
+// ---------------- IO
 
 int _nc= 0;
 
@@ -72,13 +124,12 @@ char spc() {
 L lread(); // forward
 
 L lreadlist() {
-  char r= 0, c;
-  c= spc();
-  if (!c) return -1;
+  L r;
+  char c= spc();
+  if (!c) return nil; // TODO: eof atom?
   if (c==')') return nil;
   unc(c);
   r= lread();
-  // TODO: how long list can we read with cc65?
   return cons(r, lreadlist());
 }
 
@@ -94,18 +145,23 @@ L lread() {
       c= getchar();
     }
     unc(c);
-    return n;
+    return mknum(n);
   }
-  if (isalnum(c)) { // symbol
-    char n= 0, s[32]= {0}, *p;
+  if (isatomchar(c)) { // symbol
+    char n= 0, s[MAXSYMLEN+1]= {0};
     do {
       s[n++]= c;
       c= nextc();
-    } while(c && isalnum(c) && n<32-1);
-    p= atom(s);
-    return (int)p;
+      // TODO: breaking chars: <spc>'"()
+    } while(c && isatomchar(c) && n<MAXSYMLEN);
+    printf("ATOM: %s\n", s);
+    return atom(s);
   }
-  // what is it?
+  if (c=='\'') return cons(quote, lread());
+  if (c=='"') { // string
+    assert(!"NIY: strings");
+  }
+
   printf("%%ERROR: unexpected '%c' (%d)\n", c, c);
   return -2;
 }
@@ -114,22 +170,48 @@ L eval(L x, L e) {
   return e?x:x;
 }
 
-L print(L x) {
-  printf("%d=%04x='%.32s'\n", x, x, x);
+L prin1(L x) {
+  //printf("%d=%d=%04x\n", num(x), x, x);
+  if (!x) printf("nil");
+  else if (consp(x)) {
+    L i= x;
+    putchar('(');
+    do {
+      prin1(car(i));
+      i= cdr(i);
+    } while (i && consp(i));
+    if (i) {
+      printf(" . ");
+      prin1(i);
+    }
+    putchar(')');
+  } else if (numberp(x)) {
+    printf("%d", num(x));
+  } else if (atomp(x)) {
+    printf("%s", x);
+  // TODO: strings
+  //} else if (stringp(x)) {
+  }
+  putchar(' ');
   return x;
+}
+
+L print(L x) {
+  L r= prin1(x);
+  putchar('\n');
+  return r;
 }
 
 int main() {//int argc, char** argv) {
   L r, env= nil;
-  int n;
 
   //clrscr(); // in conio but linker can't find (in sim?)
 
-  while (1) {
+  do {
     printf("65> ");
     r= eval(lread(), env);
     print(r);
-  }
+  } while (r);
 
   printf("\n\nExiting 65lisp\nBye\n\n");
   return 0;
