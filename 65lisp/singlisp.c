e7540afc760ec3389c3d7c517aefad84b47aeda8
Jonas S Karlsson <jsk@yesco.org>
2024-09-06 23:15:45 +0800

    65singlisp> eval cons or + ok, fix princ bug of list end pair

--- 65lisp/singlisp.c
+++ 65lisp/singlisp.c
@@ -15,13 +15,16 @@
 
 typedef unsigned int D;
 D nil, LAMBDA, QUOTE, T;
+D PLUS, CAR, CDR, CONS, EQ;
+
+D eval(D, D); D apply(D, D, D); D lread(); D princ(D); // forward
 
 typedef struct { D car, cdr; } Cons;
 Cons *C;
 
 void* callaign(size_t n, char bits) {
   char* p= calloc(n+1, sizeof(Cons)); // same size Atom!
-  while(((D)p & 0x03) != bits) p++;
+  while((((D)p) & 0x03) != bits) p++;
   return p;
 }
 
@@ -32,13 +35,14 @@ void* callaign(size_t n, char bits) {
 #define iscons(n) (((n)&3)==3) // ppp11
 
 D cons(D a, D d) {
+  printf("CONS: "); princ(a); putchar(' '); princ(d); putchar('\n');
   ++C;
   C->car= a;
   C->cdr= d;
   return (D)C;
 }
-#define car(c) (*(D*)c)
-#define cdr(c) (((D*)c)[1])
+#define car(c) (((Cons*)c)->car)
+#define cdr(c) (((Cons*)c)->cdr)
 
 // Atoms stored as: (D global  value, char*)
 typedef struct { D val; char* str; } Atom;
@@ -58,13 +62,14 @@ D princ(D x) {
   else if (isatom(x)) printf("%s", ((Atom*)x)->str);
   else { putchar('('); while(iscons(x)) {
       princ(car(x));
-      if (isatom(cdr(x))) { if (cdr(x)!=nil) { printf(" . "); princ(cdr(x)); } putchar(')'); break; }
-      putchar(' ');
       x= cdr(x);
-    }} return x;
+      if (iscons(x)) putchar(' ');
+    }
+    if (!iscons(x) && x!=nil) { printf(" . "); princ(x); }
+    putchar(')');
+  }  
+  return x;
 }
-
-D eval(D, D); D apply(D, D, D); D lread(); // forward
 
 char unc= 0;
 
@@ -124,12 +129,13 @@ D eval(D x, D env) {
 }
 
 D apply(D f, D x, D env) {
-  if (f==atom("car")) return car(car(x));
-  if (f==atom("cdr")) return cdr(car(x));
-  if (f==atom("cons")) return cons(car(x), car(cdr(x)));
-  if (f==atom("eq")) return car(x)==car(cdr(x))? T: nil;
-  if (f==atom("plus")) return car(x)+car(cdr(x));
-  if (f==LAMBDA) return eval(
+  //printf("APPLY: "); princ(f); printf(" ARGS= "); princ(x); putchar('\n');
+  if (f==CAR) return car(car(x));
+  if (f==CDR) return cdr(car(x));
+  if (f==CONS) return cons(car(x), car(cdr(x)));
+  if (f==EQ) return car(x)==car(cdr(x))? T: nil;
+  if (f==PLUS) return car(x)+car(cdr(x));
+  if (f==LAMBDA) return eval( // TODO: quote ?
     car(cdr(cdr(f))), bind(car(cdr(f)), x, env));
   return apply(eval(f, env), x, env);
 }
@@ -140,6 +146,12 @@ int main(int argc, char** argv) {
   C= ((Cons*)callaign(MAXCONS, 3))-1;
   nil= (D)callaign(MAXATOM, 1); car(nil)= nil; cdr(nil)= nil;
   A= 1+(Atom*)nil; LAMBDA= atom("lambda"); QUOTE= atom("quote"); T= atom("T"); car(T)= T;
+
+  CAR= atom("car"); car(CAR)= CAR;
+  CDR= atom("cdr"); car(CDR)= CDR;
+  CONS= atom("cons"); car(CONS)= CONS;
+  EQ= atom("eq"); car(EQ)= EQ; car(atom("="))= EQ;
+  PLUS= atom("+"); car(PLUS)= PLUS; car(atom("plus"))= PLUS;
 
   while(!feof(stdin)) {
     printf("65> ");
