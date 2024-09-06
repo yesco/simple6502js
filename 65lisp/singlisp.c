be2e22561e23f78c4b72e67a7e24e76fefa13ab6
Jonas S Karlsson <jsk@yesco.org>
2024-09-06 23:30:06 +0800

    singlisp.c: 73 LOCs

--- 65lisp/singlisp.c
+++ 65lisp/singlisp.c
@@ -14,13 +14,11 @@
 #define MAXATOM  1024
 
 typedef unsigned int D;
-D nil, LAMBDA, QUOTE, T;
-D PLUS, CAR, CDR, CONS, EQ;
+D nil, LAMBDA, QUOTE, T,  PLUS, CAR, CDR, CONS, EQ;
 
 D eval(D, D); D apply(D, D, D); D lread(); D princ(D); // forward
 
-typedef struct { D car, cdr; } Cons;
-Cons *C;
+typedef struct { D car, cdr; } Cons;  Cons *C;
 
 void* callaign(size_t n, char bits) {
   char* p= calloc(n+1, sizeof(Cons)); // same size Atom!
@@ -34,19 +32,12 @@ void* callaign(size_t n, char bits) {
 #define isatom(n) (((n)&3)==1) // ppp01
 #define iscons(n) (((n)&3)==3) // ppp11
 
-D cons(D a, D d) {
-  printf("CONS: "); princ(a); putchar(' '); princ(d); putchar('\n');
-  ++C;
-  C->car= a;
-  C->cdr= d;
-  return (D)C;
-}
+D cons(D a, D d) { ++C; C->car= a; C->cdr= d; return (D)C; }
 #define car(c) (((Cons*)c)->car)
 #define cdr(c) (((Cons*)c)->cdr)
 
 // Atoms stored as: (D global  value, char*)
-typedef struct { D val; char* str; } Atom;
-Atom *A;
+typedef struct { D val; char* str; } Atom;  Atom *A;
 
 D atom(char* s) {
   Atom* x= (Atom*)nil;
@@ -71,6 +62,7 @@ D princ(D x) {
   return x;
 }
 
+// poor mans ungetchar...
 char unc= 0;
 
 D readlist() {
@@ -78,25 +70,21 @@ D readlist() {
   if (isspace(c)) return readlist();
   if (c==')') return nil;
   if (c=='.') { D x= lread(); if (!unc) getchar(); unc= 0; return x; }
-  unc= c;
-  return cons(lread(), readlist()); // order of eval 1...2
+  unc= c; return cons(lread(), readlist()); // order of eval 1...2
 }
 
 D lread() {
   int n= 0, c= unc? unc: getchar(); unc= 0;
   if (isspace(c)) return lread();
   if (c<=0 || c==')') return nil;
-  while(isdigit(c)) {
-    n= n*10 + c-'0';
-    unc= c= getchar();
-    if (!isdigit(c)) return mknum(n);
+  while(isdigit(c)) { n= n*10 + c-'0';
+    unc= c= getchar(); if (!isdigit(c)) return mknum(n);
   }
   if (c=='\'') return cons(QUOTE, cons(lread(), nil));
   if (c=='(') return cons(lread(), readlist());
   { char s[32]={0}, *p= s; // overflow?
     do { *p++= c; c= getchar(); }while(c>0 && !isspace(c) && c!='(' && c!=')');
-    unc= c;
-    return atom(strdup(s));
+    unc= c; return atom(strdup(s));
   }
 }
 
@@ -104,28 +92,23 @@ D lread() {
 // --------------- lisp eval
 
 D evallist(D v, D env) {
-  if (v==nil) return nil;
-  return cons( eval(car(v),env), evallist(cdr(v),env) );
+  return v==nil? nil: cons( eval(car(v),env), evallist(cdr(v),env) );
 }
 
 D bind(D b, D v, D env) {
-  if (b==nil) return nil;
-  return cons( cons( car(b), eval(car(v),env) ),
-           bind(cdr(b), cdr(v), env) );
+  return b==nil? nil: cons( cons( car(b), eval(car(v),env) ),
+                            bind(cdr(b), cdr(v), env) );
 }
 
 D assoc(D a, D env) {
-  if (env==nil) return nil;
   if (a==car(car(env))) return car(env);
-  return assoc(a, cdr(env));
+  return env==nil? nil: assoc(a, cdr(env));
 }
 
 D eval(D x, D env) {
-  if (x==nil) return nil;
-  if (isnum(x)) return x;
+  if (x==nil || isnum(x)) return x;
   if (isatom(x)) { env= assoc(x, env); return env==nil? car(x): cdr(x); }
-  if (car(x)==QUOTE) return car(cdr(x));
-  return apply(car(x), evallist(cdr(x),env), env);
+  return car(x)==QUOTE? car(cdr(x)): apply(car(x), evallist(cdr(x),env), env);
 }
 
 D apply(D f, D x, D env) {
@@ -135,31 +118,23 @@ D apply(D f, D x, D env) {
   if (f==CONS) return cons(car(x), car(cdr(x)));
   if (f==EQ) return car(x)==car(cdr(x))? T: nil;
   if (f==PLUS) return car(x)+car(cdr(x));
-  if (f==LAMBDA) return eval( // TODO: quote ?
-    car(cdr(cdr(f))), bind(car(cdr(f)), x, env));
+  if (f==LAMBDA) return eval( car(cdr(cdr(f))), bind(car(cdr(f)), x, env));
   return apply(eval(f, env), x, env);
 }
 
 int main(int argc, char** argv) {
-  D x, r;
+  //assert(sizeof(Atom)==sizeof(Cons));
 
   C= ((Cons*)callaign(MAXCONS, 3))-1;
   nil= (D)callaign(MAXATOM, 1); car(nil)= nil; cdr(nil)= nil;
   A= 1+(Atom*)nil; LAMBDA= atom("lambda"); QUOTE= atom("quote"); T= atom("T"); car(T)= T;
 
-  CAR= atom("car"); car(CAR)= CAR;
-  CDR= atom("cdr"); car(CDR)= CDR;
-  CONS= atom("cons"); car(CONS)= CONS;
-  EQ= atom("eq"); car(EQ)= EQ; car(atom("="))= EQ;
-  PLUS= atom("+"); car(PLUS)= PLUS; car(atom("plus"))= PLUS;
+  // TODO: remove, by using dispatch system?
+  CAR= atom("car"); car(CAR)= CAR; CDR= atom("cdr"); car(CDR)= CDR;
+  CONS= atom("cons"); car(CONS)= CONS; EQ= atom("eq"); car(EQ)= EQ;
+  PLUS= atom("+"); car(PLUS)= PLUS;
 
-  while(!feof(stdin)) {
-    printf("65> ");
-    x= lread();
-    putchar('>'); princ(x); putchar('\n');
-    r= eval(x, nil);
-    princ(r); putchar('\n');
-  }
+  while(!feof(stdin)) { printf("65> "); princ(lread()); putchar('\n'); }
 
   return argv? argc-1: 0; // BS
 }
