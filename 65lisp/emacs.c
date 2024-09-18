60a8e3ab43acb3a4d178c7adf12a2624fd9ab320
Jonas S Karlsson <jsk@yesco.org>
2024-09-18 13:45:01 +0800

    65emacs: clarify code and doc

--- 65lisp/emacs.c
+++ 65lisp/emacs.c
@@ -3,6 +3,9 @@
 //
 // Edits a multiline string inside a given buffer.
 //
+//
+// MOVEMENT
+//
 //   CTRL-B    - back one char
 //   CTRL-F    - forward one char
 //
@@ -15,14 +18,22 @@
 //   ESC <     - beginning of file (ESC v)
 //   ESC >     - end of file (CTRL-V)
 //
+//
+// EDITING
+//
 //   Backspace - del previous char
 //   CTRL-D    - del next char
 //
-//   CTRL-O    - insert line before cursor (\n CTRL-B)
+//   CTRL-O    - insert new line before cursor (\n CTRL-B)
 //
-//   CTRL-K    - kill (cut till end of line, or on end, cut end)
-//   CTRL-Y    - Yank all cuttings stored
+//
+// CUT'N'PASTE
+//
+//   CTRL-K    - kill (cut till end of line, or at end, cut end)
+//   CTRL-Y    - Yank *all* cuttings stored
 //   CTRL-G    - clear yank buffer
+//
+// OTHER
 //
 //   CTRL-X/C  - exit
 
@@ -30,10 +41,9 @@
 // - keep x-position of previous row when go up/down
 //
 // Limitations: (for now)
-// - lines must fit on screen
+// - lines must fit on screen (or will wrap)
 // - doesn't grow buffer
 // - redraws screen every key stroke (LOL)
-
 
 #include <stdio.h>
 #include <string.h>
@@ -46,17 +56,18 @@
 // TODO: resize
 void edit(char* e, size_t size) {
   char *p= e, *cur= e, *sl, *el, *last= e+size-1, k;
-  int x= 0, yank= 0, elen, curlen;
+  int xpos= 0, yank= 0, elen, curlen;
 
   *last= 0; // end used as yank buffer
   do {
     // update screen
     clrscr();
     if (cur!=e) printf("%.*s", (int)(cur-e), e);
-    printf("[s"); // save cursor
+    printf("[s"); // save cursor // TODO: only vt100
     if (cur<e+size) printf("%s<<<\n", cur);
+
+    // show yank buffer if not empty
     if (*last) {
-      // show yank buffer if not empty
       printf("\n\nYANK>>>");
       p= last; yank= 0;
       while(*p) putchar(*p--),yank++;
@@ -64,19 +75,21 @@ void edit(char* e, size_t size) {
     }
     //printf("\n: cur=%d size=%d yank=%d size=%d\n", cur-e, strsize(e), yank, size);
     revers(1); printf("65EMACS: ESC< ^APNBFOKYEG ESC> ^X\n"); revers(0);
-    printf("[u"); // restore
+
+    printf("[u"); // restore cursor // TODO: only vt100
 
     // start of line
     sl= cur; while(sl>e && sl[-1]!='\n') --sl;
     el= cur; while(*el && *el!='\n') ++el;
+
+    // adjust for x column ctrl-P and ctrl-N (must be here after new sl/el)
+    if (xpos) { cur= (sl+xpos<el)? sl+xpos: el; xpos= 0; continue; } // update cursor
+
     elen= strlen(e);
     curlen= elen + cur-e;
 
-    // adjust for x column ctrl-P and ctrl-N
-    if (x) { cur= (sl+x<el)? sl+x: el; x= 0; continue; } // update cursor
-
-    // process key
-    x= 0;
+    // -- process key
+    xpos= 0;
     k= getchar();
     if (k==27) k= 128+getchar()-32;
     switch(k) {
@@ -84,11 +97,11 @@ void edit(char* e, size_t size) {
     // movement // TODO: simplier, make a search fun?
     case META+'<': case META+'V':cur= e; break;
     case META+'>': case CTRL+'V':cur= e+elen; break;
-    case CTRL+'P': x= cur-sl; cur= sl-1; break;
+    case CTRL+'P': xpos= cur-sl; cur= sl-1; break;
     case CTRL+'A': cur= sl; break;
     case CTRL+'B': --cur; break;
     case CTRL+'F': ++cur; break;
-    case CTRL+'N': x= cur-sl; cur= el+1; break;
+    case CTRL+'N': xpos= cur-sl; cur= el+1; break;
     case CTRL+'E': cur= el; break;
 
     // exit/other
