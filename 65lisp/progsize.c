fffbdbfa9e5695d6fb09db2dde6cb8724335cd9b
Jonas S Karlsson <jsk@yesco.org>
2024-09-18 15:28:18 +0800

    fix

--- 65lisp/progsize.c
+++ 65lisp/progsize.c
@@ -4,7 +4,7 @@
   char firstbss;
 
   #undef PROGSIZE
-  #define PROGSIZE printf("Program size: %u bytes(ish)\n", ((unsigned int)&firstbss)-((unsigned int)startprogram))
+  #define PROGSIZE printf("\nProgram size: %u bytes(ish)\n", ((unsigned int)&firstbss)-((unsigned int)startprogram))
 
   //char firstvar= 0;
   //#define PROGRAMSIZE printf("Program size: %u bytes(ish) %04x-%04x %04x %04x\n", ((uint)&firstbss)-((uint)startprogram), (uint)startprogram, (uint)main, (uint)&firstvar, (uint)&firstbss)
