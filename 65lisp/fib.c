28822723e0e9487eee9e0a28d60ac53225d72ff7
Jonas S Karlsson <jsk@yesco.org>
2024-10-05 19:39:08 +0700

    just make it work on linux

--- 65lisp/fib.c
+++ 65lisp/fib.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#include <conio.h>
+//#include <conio.h>
 
 extern int print(int n) { printf("\n%d", n); return n; }
 
-extern int nil=0, T=0;
+//extern int nil=0, T=0;
 
 long calls= 0;
 
@@ -26,8 +26,8 @@ int fibn(int n) {
   else return fib(n-1) + fib(n-2);
 }
 
-extern unsigned int asmfib(unsigned int n);
-extern unsigned int fibinline(unsigned int n);
+//extern unsigned int asmfib(unsigned int n);
+//extern unsigned int fibinline(unsigned int n);
 
 //extern unsigned int asmltfib(unsigned int n);
 
@@ -49,18 +49,19 @@ unsigned int ufib(unsigned int n) {
 }
 
 int main() {//int argc, char** argv) {
-  long bench= 3000; // 43.31s
+  // tablet is (/ 42000 1.37) 30700x faster!
+  long bench= 3000*42000/1.37; // 43.31s 
   long i= bench;
   //long bench= 30000; // 432s
   int n= 8;
   long z= 0;
   int r= 42;
   while(--i) {
-    //r= fib(n);	// 63 B    43.31s
+    r= fib(n);	// 63 B    43.31s
     //r= ufib(n);	//         43.31s
     //r= ltfib(n);	//  54 B - 41.53s "best of cc65"
     //r= asmfib(n);	//  39 B   29.17s handwritten
-    r= fibinline(n);	// 114 B   21.45s inlined "all" (63 lines)
+    //r= fibinline(n);	// 114 B   21.45s inlined "all" (63 lines)
     z+= r;
   }
   printf("fib %d => %d z=%ld calls=%ld   %ld c/bench\n", n, r, z, calls, calls/bench);
