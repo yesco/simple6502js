6517e3237bedadcfed7257318f7fe2527d5f4852
Jonas S Karlsson <jsk@yesco.org>
2024-10-10 13:44:36 +0200

    65asm prepare to merge asm

--- 65lisp/asm.c
+++ 65lisp/asm.c
@@ -4,82 +4,84 @@
 
 //#include <conio.h>
 
-#define O(op) op
-#define O2(op, b) op "#"
-#define N2(name, op, b) O2(op, b)
+#include "progsize.c"
 
-#define O3(op, w) op "??"
+#define mO(op) op
+#define mO2(op, b) op "#"
+#define mN2(name, op, b) mO2(op, b)
 
-#define LDAn(n) N2("LDAn","\xA9",n)
-#define LDXn(n) N2("LDXn","\xA2",n)
-#define LDYn(n) N2("LDYn","\xA0",n)
+#define mO3(op, w) op "??"
 
-
-#define LDA(w)  O3("\xAD",w)
-#define LDX(w)  O3("\xAE",w)
-#define LDY(w)  O3("\xAC",w)
-
-#define STA(w)  O3("\x8D",w)
-#define STX(w)  O3("\x8E",w)
-#define STY(w)  O3("\x8C",w)
+#define mLDAn(n) mN2("LDAn","\xA9",n)
+#define mLDXn(n) mN2("LDXn","\xA2",n)
+#define mLDYn(n) mN2("LDYn","\xA0",n)
 
 
-#define ANDn(b) O2("\x29",b)
-#define ORAn(b) O2("\x09",b)
-#define EORn(b) O2("\x49",b)
+#define mLDA(w)  mO3("\xAD",w)
+#define mLDX(w)  mO3("\xAE",w)
+#define mLDY(w)  mO3("\xAC",w)
 
-#define ASL()   O("\x0A")
-#define CMPn(b) N2("CMPn","\xC9",b)
-#define CPXn(b) N2("CPXn","\xE0",b)
-#define CPYn(b) N2("CPYn","\xC0",b)
-
-#define SBCn(b) N2("SBC","\xE9", b)
-
-#define PHP()   O("\x08")
-#define CLC()   O("\x18")
-#define PLP()   O("\x28")
-#define SEC()   O("\x38")
-
-#define PHA()   O("\x48")
-#define CLI()   O("\x58")
-#define PLA()   O("\x68")
-#define SEI()   O("\x78")
-
-#define DEY()   O("\x88")
-#define TYA()   O("\x98")
-#define TAY()   O("\xA8")
-#define CLV()   O("\xB8")
-
-#define INY()   O("\xC8")
-#define CLD()   O("\xD8")
-#define INX()   O("\xE8")
-#define SED()   O("\xF8")
-
-#define TXA()   O("\x8A")
-#define TAX()   O("\xAA")
-
-#define BMI(b)  O2("\x30", b)
-#define BPL(b)  O2("\x10", b)
-
-#define BNE(b)  O2("\xD0",b)
-#define BEQ(b)  O2("\xF0",b)
-
-#define BCC(b)  O2("\x90",b)
-#define BCS(b)  O2("\xB0",b)
-
-#define BVC(b)  O2("\x50",b)
-#define BVS(b)  O2("\x70",b)
+#define mSTA(w)  mO3("\x8D",w)
+#define mSTX(w)  mO3("\x8E",w)
+#define mSTY(w)  mO3("\x8C",w)
 
 
-#define JSR(a)  N3("JSR","\x20",a)
-#define RTS(a)  O("\x60")
+#define mANDn(b) mO2("\x29",b)
+#define mORAn(b) mO2("\x09",b)
+#define mEORn(b) mO2("\x49",b)
 
-#define JMP(a)  N3("JMP", "\x4c",a)
-#define JMPi(a) N3("JPI", "\x6c",a)
+#define mASL()   mO("\x0A")
+#define mCMPn(b) mN2("CMPn","\xC9",b)
+#define mCPXn(b) mN2("CPXn","\xE0",b)
+#define mCPYn(b) mN2("CPYn","\xC0",b)
 
-#define BRK(a)  O("\x00")
+#define mSBCn(b) mN2("SBC","\xE9", b)
 
-char bi=0, buff[255];
+#define mPHP()   mO("\x08")
+#define mCLC()   mO("\x18")
+#define mPLP()   mO("\x28")
+#define mSEC()   mO("\x38")
+
+#define mPHA()   mO("\x48")
+#define mCLI()   mO("\x58")
+#define mPLA()   mO("\x68")
+#define mSEI()   mO("\x78")
+
+#define mDEY()   mO("\x88")
+#define mTYA()   mO("\x98")
+#define mTAY()   mO("\xA8")
+#define mCLV()   mO("\xB8")
+
+#define mINY()   mO("\xC8")
+#define mCLD()   mO("\xD8")
+#define mINX()   mO("\xE8")
+#define mSED()   mO("\xF8")
+
+#define mTXA()   mO("\x8A")
+#define mTAX()   mO("\xAA")
+
+#define mBMI(b)  mO2("\x30", b)
+#define mBPL(b)  mO2("\x10", b)
+
+#define mBNE(b)  mO2("\xD0",b)
+#define mBEQ(b)  mO2("\xF0",b)
+
+#define mBCC(b)  mO2("\x90",b)
+#define mBCS(b)  mO2("\xB0",b)
+
+#define mBVC(b)  mO2("\x50",b)
+#define mBVS(b)  mO2("\x70",b)
+
+
+#define mJSR(a)  mN3("JSR","\x20",a)
+#define mRTS(a)  mO("\x60")
+
+#define mJMP(a)  mN3("JMP", "\x4c",a)
+#define mJMPi(a) mN3("JPI", "\x6c",a)
+
+#define mBRK(a)  mO("\x00")
+
+unsigned char bi=0, bz=0, buff[255];
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
@@ -99,11 +101,14 @@ void __cdecl__ addasm(char* x, uchar len, ...) {
   for(bi=0 ;bi<len; ) {
     c= x[bi];
 
-    if (c=='#') { buff[bi]= ARG(n); ++n; }
-    else if (c=='?') { buff[bi++]= ARG(n); buff[bi]= ARG(n) >> 8; ++n; }
-    else buff[bi]= c;
+    if (c=='#') { buff[bz]= ARG(n); ++n; }
+    else if (c=='?') { buff[bz]= ARG(n); ++bz; ++bi; buff[bz]= ARG(n) >> 8; ++n; }
+    else buff[bz]= c;
+ 
+    ++bz;
+    buff[bz]= 0; // BRK, haha
 
-    buff[++bi]= 0; // BRK, haha
+    ++bi;
   }
   printf("ASM[%d]=> \"%s\"\n", len, buff);
 }
@@ -114,11 +119,17 @@ void __cdecl__ addasm(char* x, uchar len, ...) {
 
 int main(int argc, char** argv) {
   printf("Hello " "\n\"22:\x22 \n#23:x23 \n27:\x27 \n+2B:\x2B \n/2F:\x2F \n32:\x32 \n33:\x33 \n34:\x34 \n37:\x37 \n:3a:\x3a \n;3b:\x3b \n<3c:\x3c \n?3f:\x3f World!\n");
-  ASM(TYA() TXA() LDAn(41), 0);
-  ASM(TYA() TXA() LDAn(41), 65, 0);
-  ASM(TYA() TXA() LDAn(41) LDA(99), 65, 256*65+66);
-  ASM(TYA() TXA() LDAn(41) LDA(99), 1,2,3,4,5,6,7,0xabcd,0xdeadbeef);
+  bz=0; ASM(mTYA() mTXA() mLDAn(41), 0);
+  bz=0; ASM(mTYA() mTXA() mLDAn(41), 65, 0);
+  bz=0; ASM(mTYA()
+      mTXA()
+      mLDAn(41)
+      mLDA(??)
+      , 65, 256*65+66);
 
   //printf("foo: %s\n", MAKE_STR(65, 66, 67, 68));
+
+  PROGSIZE;
+
   return 0;
 }
