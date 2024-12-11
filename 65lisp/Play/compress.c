3c0366f9902f2ae6bfd11a6a7726d933554f2657
Jonas S Karlsson <jsk@yesco.org>
2024-12-12 03:51:00 +0800

    compress: 61%, but decompress forked, lol

--- 65lisp/Play/compress.c
+++ 65lisp/Play/compress.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 // characters ordered in terms of frequency - "Oxford stuff"
 // " eariotnslcudpmhgbfywkvxzjq"
@@ -58,12 +61,20 @@ Of 1,699,542,842 trigrams scanned:
 //   two spaces (or any nibble 0) can NOT be encoded as '  ',
 //   but as ' U ', for more, see REPEPAT
 
-char  core[16]    = " eariotnslcSNUPD"; // U=Upcase next letter (auto after .), "SN PD" - shifters
-char   Ucore[16]  = " EARIOTNSLCsnupd"; //   Upcase letters, 'Uu' => toggle sticky Upcase "sn pd" - shifters
+#define CORE       " eariotnslcUSNPD" // U=Upcase next letter (auto after .), "SN PD" - shifters
+#define COREs      " eariotnslc     " // U=Upcase next letter (auto after .), "SN PD" - shifters
+#define  UCORE     " EARIOTNSLC     " //   Upcase letters, 'Uu' => toggle sticky Upcase "sn pd" - shifters
+#define SECOND     "udpmhgbfywkvxzjq" // the rest of alphabet
+#define  USECOND   "UDPMHGBFYWKVXZJQ" //   upcase 'US?'
+
+char chars[]      = COREs UCORE SECOND USECOND;
+
+char  core[16]    = CORE;
+char   Ucore[16]  = UCORE;
 
 // These are secondary maps
-char  Second[16]  = "udpmhgbfywkvxzjq"; // the rest of alphabet
-char   USecond[16]= "UDPMHGBFYWKVXZJQ"; //   upcase 'US?'
+char  Second[16]  = SECOND;
+char   USecond[16]= USECOND;
 char  Nums[16]    = " 0123456789.,+-E"; // numbers, sticky till '\0' (not give space), prefix U - nonstick
 char  Punct[16]   = ".,:;-'\"/<>(){}[]"; // ., assumes spaces before, unless 'U', 2 nilbbles 'P.'
 char   UPunct[16] = "?!#%_`&=@^|~\\\n\t\b"; // ?! assumes space after, 'UP?', \h=backspace!
@@ -100,7 +111,119 @@ char* Seqs[16]= {
   "&nbsp;", "&lt;", "&gt;", "&amp;", "&quot;",    // 5 15
   "fuck",                                         // 1 16 - for English
 };
+
+char* maps[]= { core, Ucore, Second, USecond, Nums, Nums, Punct, UPunct, (char*)Dict, (char*)Dict };
+
+
+typedef unsigned char uchar;
   
+char* compress(char* s) {
+  char *r= calloc(strlen(s)*2, 1), *p=r;
+  char c,*m;
+  uchar j,nibble;
+  uchar U,map;
+  uchar C=0, o=0,i=2;
+
+#define OUTNIBBLE(nibble) do { \
+    o|= (nibble); \
+    printf("  [c='%c' $%02X U=%d map=%d] => nibble %d\to=%2x", c, c, U, map, nibble, o); \
+    if (--i) o<<= 4; \
+    else { printf("   => %02x", o); *++p=o,o=0,i=2; } \
+    putchar('\n'); \
+    } while (0);
+
+  --p;
+  while((c= *s)) {
+    // TODO: prefix string match first
+
+    // find map of char
+    m= strchr(chars, c);
+    if (!m) {
+      printf("-- %%ERROR: can't find char '%c' (%d, %02x)\n", c, c, c);
+      exit(1);
+    }
+
+    // Encode
+    j= m-chars;
+    nibble= j&15;
+    map= j>>4;
+    U= map&1;
+
+    if (U) { putchar(' '); OUTNIBBLE(12); }
+    if (map) { putchar(' '); OUTNIBBLE(12+map); }
+    OUTNIBBLE(nibble);
+    ++s;
+  }
+  
+  OUTNIBBLE(0); OUTNIBBLE(0);
+  if (i) OUTNIBBLE(0);
+  return realloc(r, strlen(r)+1);
+}
+
+char* decompress(char* cs) {
+  char *r= calloc(strlen(cs)*4, 1), *o= r;
+  uchar* s= (uchar*)cs;
+  // states
+  char i= 2, m= 0, C= 0, U= 0; // CapsLock
+  uchar nibble, c;
+  char* map;
+
+  while (*s && i) {
+    nibble= --i? *s>>4: *s&15;
+    //printf("[nibble=%2d] ", nibble);
+    if (!i) ++s,i=2;
+
+    // Change map?
+    if (m<2 && nibble>=12) {
+      // 'U' upcase & caps-lock
+      if (nibble==12) {
+        if (U) C=1-C,U=0; // toggle U if 'UU'
+        else U= 1-C; // opposite Caps
+        m= 0;
+      } else m= (nibble-12)*2; // select map SNPD
+
+      // get next nibble
+      continue; 
+    }
+
+    // get char
+    map= maps[m+U];
+    c= map[nibble];
+
+    printf("[i=%d C=%d U=%d m=%d nibble=%2d] => '%c'\n", i, C, U, m+U, nibble, c);
+
+    U=0;
+  }
+
+  // adjust to actual size
+  return realloc(r, strlen(r)+1);
+}
+
+//char* d= decompress("\x11\xc1\x02\xc2\x0c\x20");
+
 int main(void) {
+  //char *s= "eE aAAaa foo";
+  //char *s= "Hello My name is Jonas S Karlsson";
+  char *s= "hello my name is jonas s karlsson";
+  char *c, *d, *p;
+  int sl,cl,dl;
+
+  printf("TEXT[%3d]: >%s<\n\n", sl= (int)strlen(s), s);
+
+  c= compress(s);
+  printf("COMP[%3d]: >", cl= (int)strlen(c), c);
+  p= c;
+  while(*p) printf("%02x ", *p++);
+  printf("<\n\n");
+
+  d= decompress(c);
+  printf("DCMP[%3d]: >%s<\n\n", dl= (int)strlen(d), d);
+
+  printf("\n>%s:%d:%d:%d%%\n", s, sl, cl, (cl*10000+4000)/sl/100); // 4000? why not 5000?
+  printf(">%s:\n", d);
+
+  if (dl!=sl) { printf("--CONVERSION FAILED!\n"); }
+  //assert(dl==sl);
+  
   return 0;
 }
