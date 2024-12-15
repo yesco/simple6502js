eb40d075c7ef7b831a2b1cff67c76d812f45a337
Jonas S Karlsson <jsk@yesco.org>
2024-12-15 17:59:44 +0800

    compress: update savings

--- 65lisp/Play/dictsavings.pl
+++ 65lisp/Play/dictsavings.pl
@@ -1,15 +1,31 @@
 open(IN, "./Play/wordfreq |");
+$i= 0;
 while (<IN>) {
     if (/^\s*(\d+)\s+(\w+)/) {
         my ($n, $w) = ($1, $2);
+        $i++;
         # nibbles
-        $S= 0;
+        $N= 0;
         foreach $c (split("", $w)) {
-            $S++ if $c =~ /[eariotnslcd]/; 
+            if ($c =~ /[ eariotnslcdu]/) {
+                $N++;
+            } elsif ($c =~ /[pmhgfyw<\/>.,"]/) {
+                $N+= 2;
+            } else {
+                # quoted
+                $N+= 3;
+            }
         }
-        $savings= $n * (length($w)+2+$S) - 2 + $S;
+
+        # WTF? - maybe I was drunk?
+
+        # bytes
+        #   $N nibbles for the word, 2 for preceeding/succeeding spaces, -2 for 'D?' entry
+        $savings= $n * ($N+2-2-(($i>14)?1:0))/2;
+        $save128= $n * (length($w)+2-1);
 
         printf("%8d %8d\t%s\n", $savings, $n, $w);
+        #printf("%8d %8d\t%s\n", $save128, $n, $w);
     } else {
         print STDERR "--ERROR: ", $_;
     }
--- 65lisp/Play/ngram.pl
+++ 65lisp/Play/ngram.pl
@@ -3,5 +3,5 @@ $s = join("", <>);
 #while($s =~ /(....)/g) {
 $len= length($s);
 for $i (0..$len) {
-    print '"', substr($s,$i,10), '"', "\n";
+    print '"', substr($s,$i,1), '"', "\n";
 }
