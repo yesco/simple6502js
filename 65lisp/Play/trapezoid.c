93104ca4111b073f5512da9997a373f6d2ce77c8
Jonas S Karlsson <jsk@yesco.org>
2025-02-06 20:26:47 +0700

    trapezoid: with mask texture!

--- 65lisp/Play/trapezoid.c
+++ 65lisp/Play/trapezoid.c
@@ -17,20 +17,50 @@ void main() {
     gcurx= 40; gcury= 150; draw(180, -15, 2);
 
     /// xor down to fill! clevef simple!
-    if (1) {
+    switch(2) {
+
+    case 1: // xor every byte only (no fill)
+      for(c=0; c<40; ++c) {
+        p= HIRESSCREEN-40+c;
+        v= 0;
+        for(r=0; r<200; ++r) {
+          p+=40;
+          *p^= 63;
+        }
+      } break;
+
+    case 2: { // xor fill all screen
+      char mask[]= {1+4+16 +64+128, 2+8+32 +64+128};
+      #define MASKMASK 0b01
+
       for(c=0; c<40; ++c) {
         p= HIRESSCREEN-40+c;
         v= 0;
         for(r=0; r<200; ++r)
           *p= v= (*(p+=40)^v)|64;
       }
-    } else {
+    } break;
+
+    case 3: { // xor fill with MASK all screen
+      char mask[]= {1+4+16 +64+128, 2+8+32 +64+128};
+      #define MASKMASK 0b01
+
+      for(c=0; c<40; ++c) {
+        p= HIRESSCREEN-40+c;
+        v= 0;
+        for(r=0; r<200; ++r)
+          *p= (v= (*(p+=40)^v)|64) & mask[r & MASKMASK];
+      }
+    } break;
+
+    case 4: // xor fill limited area
       for(c=40/6-1; c<=(40+180)/6+1; ++c) {
         p= HIRESSCREEN + 50*40-40 + c;
         v= 0;
         for(r=50; r<150; ++r)
           *p= v= (*(p+=40)^v)|64;
       }
+      break;
     }
 
     if (0)
