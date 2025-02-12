71882163c4439dc43a47380ac5a376c13dcb3248
Jonas S Karlsson <jsk@yesco.org>
2025-02-13 02:04:56 +0700

    bits: B_11_11__1...

---
+++ 65lisp/Play/genbits.pl
@@ -0,0 +1,10 @@
+print "#define B(a,b,c,d,e,f,g,h) (128*a+64*b+32*c+16*d+8*e+4*f+2*g+h)\n";
+print "\n";
+print "#define _ 0\n";
+
+for $i (0..255) {
+    $b= sprintf("%08b", $i);
+    $d= $b;
+    $d=~ s/0/_/ge;
+    print sprintf("#define B_$d     %3d\n", $i);
+}
