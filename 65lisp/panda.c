d9cf0190735d66ed3e261e9b1dfb921057d21d31
Jonas S Karlsson <jsk@yesco.org>
2025-04-21 20:19:29 +0700

    panda: sql functions?

--- 65lisp/panda.c
+++ 65lisp/panda.c
@@ -114,7 +114,7 @@ void op(char c, char* cmd) {
   lastop= c;
 }
 
-// make parser to output objectlog?
+// TODO: make parser to output objectlog?
 void panda(char* cmd) {
   char c;
   // TOOD: set/reset (string) stack-heap
@@ -129,12 +129,12 @@ void panda(char* cmd) {
   switch(c= *++cmd) {
   case 0:   is[nv]= 1; op(0, cmd); RESULT(); return;
     // formatting
-  case ';': op(0, cmd); is[nv]= 1; goto next; // TODO: no space
-  case ',': op(0, cmd); is[nv]= 1; goto next; // TODO: tab?
-  case ' ': goto next; // TODO: space/formatted (?)
+  case ' ': goto next;
+  case ';': op(0, cmd); goto next;            // no value
+  case ',': op(0, cmd); is[nv]= 1; goto next; // value
 
-    // print string 
-    // TODO: accumulate/set as output value
+    // print string
+    // TODO: accumulate/set as output value in parsing stake
 #ifdef FOO
     // TODO: NOT CORRECT!
   case '"': case '\'': {
@@ -147,6 +147,7 @@ void panda(char* cmd) {
     // pos ref
   case '$': v[++nv]= var(*++cmd); goto next;
 
+    // numbers and words
   default:
     // TODO: remove op(0) ?
     if (c>='0' && c<='9') { v[++nv]= c-'0';
@@ -199,3 +200,139 @@ int main(void) {
 
   return 42;
 }
+
+/*
+  from minisquel/object.txt
+
+  about 70 functions
+
+CONTROL
+
+t	true, done/exit
+f	fail, exit
+j	jump relative
+g	goto absolute
+o	OR
+	AND = imlicit!
+	
+:=	set variable
+
+
+NUMBERS
+
++
+-
+*
+/
+%
+^	a^b / pow / **
+
+|	or bitwise (52 bits)
+&	and bitwise (52 bits)
+xo	xor bitwise (52 bits)
+
+N=
+N!
+
+
+COMPARE
+
+!=	<>
+=	==
+<	
+>
+<=	!>
+>=	!<
+
+
+LOOPS
+
+i 	to
+i	iota
+i	fromto
+
+i B E		iota: i 1 10= 1..10
+d B E S		dota: i 1 10 0.1
+l F f...	line: l FILE field ...
+
+F FILE fname	FILE, fopen(fname)
+
+
+PRINTING
+
+ou	out "columns" w header (first time)
+.	print list of values formatted
+p	print no formatting
+n	newline
+
+
+STRINGS
+
+S=
+S!
+
+CO	concat
+AS	ascii
+CA	char
+CI	charindex
+LE	left
+RI	right
+LO	lower
+UP	upper
+LN	length
+LT	ltrim
+RT	rtrim
+TR	trim
+ST	str
+
+li	like
+il	ilike
+
+ts	timestamp
+
+
+
+XML EXTRACTION FUNCTIONS
+
+xt	xmlpath
+xt	jsonpath        duckdb: foo->'$foo.bar[2].fie'  ->> for VARCHAR
+xt      extract
+xt      get
+
+
+
+MATH FUNCTIONS
+
+sr	sqrt
+si	sin
+co	cos
+ta	tan
+
+ab	abs
+ac	acos
+as	asin
+at	atan
+a2	atan2
+cr	dbrt
+ce	ceil
+de	degrees
+fl	floor
+ep	exp
+
+ie	isfinite
+ii	isinf
+in	isnan
+
+ln	log
+lg	log10
+l2	log2
+
+pi	PI
+po	pow ** ^
+
+rd	radians
+ra	random
+ri	srandom/init
+sg	sign
+
+*/
