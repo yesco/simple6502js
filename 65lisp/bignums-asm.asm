bb591a546f9f22e9c4ae1b998f2c6bb41e038972
Jonas S Karlsson <jsk@yesco.org>
2025-07-03 02:47:54 +0700

    bignums: _bigshl

--- 65lisp/bignums-asm.asm
+++ 65lisp/bignums-asm.asm
@@ -14,8 +14,14 @@ tos:    .res 2
 PRINTHEX=1
 .include "print.asm"
 
-anum:   .byte 1,0
-bnum:   .byte 1,$ab
+.data
+
+anum:   .byte 1,0     
+        .res 256
+bnum:   .byte 1,$ab 
+        .res 256
+
+.code
 
 .macro TOS num
         lda #<num
@@ -32,22 +38,34 @@ _initlisp:
         NEWLINE
         
         TOS bnum
-        jsr _bigprint
+double: 
+;        jsr _bigprint
+;        NEWLINE
+        jsr _bigshl
+        jmp double
+
         NEWLINE
+        putc 'E'
+        putc 'N'
+        putc 'D'
 
 halt:   jmp halt
 
 ;;; ========================================
 ;;;               B I G N U M S
 
-_bigprint:        
-        lda #'$'
-        jsr putchar
-
+.proc bignum 
         ldy #0
         lda (tos),y
         tay
-        
+        rts
+.endproc
+
+.proc _bigprint
+        lda #'$'
+        jsr putchar
+
+        jsr bignum
 next:   
         lda (tos),y
         jsr print2h
@@ -55,8 +73,43 @@ next:
         bne next
         
         rts
+.endproc
 
-_bigadd:  
+.proc _bigshl
+        ldy #0
+        lda (tos),y
+        sta savey
+        jsr print2h
+
+        clc
+next:   
+        iny
+        lda (tos),y
+        adc (tos),y
+        sta (tos),y
+        dec savey
+        bne next
+        
+        ;; extend?
+        bcc ret
+        iny
+        lda #0
+        adc #0
+        sta (tos),y
+
+        ldy #0
+        lda (tos),y
+        clc
+        adc #1
+        sta (tos),y
+
+ret:
+        rts
+.endproc
+
+        
+;;; 1 byte 2 digits
+; (+ (* 9 16) 9) = 153        
 
         
         
