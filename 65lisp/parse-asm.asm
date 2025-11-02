57a529a08b2abbad1e66512523ed27200c4c4f8a
Jonas S Karlsson <jsk@yesco.org>
2025-11-02 08:48:24 +0700

    parse: RECURSION works, optimized, 8%ish faster than cc65

--- 65lisp/parse-asm.asm
+++ 65lisp/parse-asm.asm
@@ -1045,6 +1045,330 @@ FUNC _runtimestart
 
 ;;; TODO: IRQ put here!
 
+
+;;; support RECURSION, has RUNTIME code overhead
+;;; of about 63 B for optimizing restore and make
+;;; function exit using RTS.
+;;; 
+;;; Alternative cost is (+ 21 12) == 33 B !
+;;; overhead per recursive function!
+;
+RECURSION=1
+
+
+
+
+.ifdef RECURSION
+;;; restory                       13 B
+;;;       (but calling overhead 9 B)
+;;;       (only improvement is RTS in user code
+;;;        and no jmp "end" function to do cleanup)
+
+;;;               OR
+
+;;; (+ 24     27       12         ) = 63 ~ 69 B
+;;;    swapY  restore8 r6,r4,r2
+
+
+
+
+;;; Implementing support for recursion in cc02
+;;; (see Play/4params.c for example)
+;;; 
+;;;        F+main
+;;; cc65:  125 B  475c/call ( 356.sim 440.tap )
+;;; vbcc:  242 B
+;;; MeoC: ~138 B            (estimate using _X rule
+;;;        255 B            (+33 B loop)
+;;;        255 B  709c !    WORKS: enter+exit==swap
+;;;               554c      enter=swap, exit=restore
+;;;                            12.4 % slower than cc65
+;;;               503c      restoryY
+;;;        23x B  496c      JSK_CALLING (save 10B/call)
+;;;        22x B  503c      OPTJSK_CALLING (19 calls)
+;;;        17x B  436c      restore8 (RUNTIME: + 27 B)
+;;;                             3 calls saves 30 B
+;;;                           8.2% faster than cc65
+;;;        174 B  456c      swapY: 24 B (204-30 loop)
+;;;                           saved itself!
+;;; 
+;;;    (maybe not worth it, the last ...)
+;;;        172 B  407c      swap8    (RUNTIME: + 97 B)
+;;;                            +2 calls saves cost
+;;;                           14% faster than cc65
+;;;                             RUNTIME==160 B :-(
+;;; 
+
+;
+JSK_CALLING=1
+
+;;; make cleanup happen automatic after JSR!
+;;; (push addr of cleanup, & number 8)
+;;; Smaller code as well as faster!
+;
+OPTJSK_CALLING=1
+
+;;; adds +97 B, improves PARAM4 436c => 407c
+;;; maybe NOT WORKTH IT
+;;; (14% faster than cc65)
+;CALLSWAP8=1
+
+
+
+.ifdef JSK_CALLING
+;;; must be part of RUNTIME for these optimizations
+
+
+;;; 1078241
+;;; 12507300 (/ (- 12507300 1078241) 1000 23) = 496
+;;; using JSK_CALLING! 267 => 279 bytes?
+;;; uses extra 6c (+6B jumps) but saves
+
+;;; OPTJSK_CALLING
+;;; 1076977
+;;; 10755024  (/ (- 10755024 1076877) 1000 19)
+;;;    509c / call (+ 13c)
+;;;    
+;;; 937422
+;;; 10279469 (/ (- 10279469 937422) 1000 19) = 491
+;;;    491c / calll (net -5c!)
+;;;  WRONGWONGWOGNOWGNOWGNOWGNWOGNWOGN
+
+;;; restoreY (pha 8)
+;;; (/ (- 10508495 937422) 1000 19) = 503
+
+;;; for RECURSIVE functions... (do we care?)
+;;; 
+;;; 279 B before incl OPTJSK_CALLING
+;;; 
+;;; 258 B doing restore8:  (- 21 B)
+;;;    3 function calls saves in +27 B restore8
+;;; 
+;;; 240 B doing swap8: (- 18 B)
+;;;    6 function calls saves in +97 B swap8
+;;; BUT: => 14% faster than cc65...
+;;; 
+ 
+;;; restore8 !!! (+ 27 B) (incl restore6,4,2)
+;;; 9235009 (/ (- 9235009 937422) 1000 19) = 436
+
+;;; swap8:  ! (+ 97 B)
+;;; 8681299 (/ (- 8681299 937422) 1000 19) = 407 !!
+;;; 
+;;; cc65: 475 c / call
+;;; 
+;;; (/ 407 475.0) => 14 % faster than cc65
+
+
+
+;;; 22 max: (* 22 (+ 8 2)) = 220 !
+;        .byte "  r= F(22, 0, 1, 65535);",10
+;        .byte "  r= F(3, 0, 1, 65535);",10
+
+;;;  43953 c overhead/start
+;;; 692453 compile
+;;; 732569 (- 732569 692453 43953) 1x
+;;; 
+
+;;;    0                 1           10      100
+;;; 791254 compile    820571       820695    820942
+;;; 834224 run 0      860660       865860   1424480
+;;; 
+;;; (/ (- 1424480 820942) 100) = 6035c !!!!????
+
+;;;  1065057
+;;;  1695899 (/ (- 1695899 1065057) 1000) = 630
+;;; 13821630 (/ (- 13821630 1065057) 1000 23) = 554!!!
+;;; 
+;;; 554! cycles, so ok (/ 554 493.0) =
+;;;       12.4% slower than cc65
+;;;  1072631
+;;; 13821624 (/ (- 13821624 1072631) 1000 23) = 554
+
+.ifdef CALLSWAP8
+
+.macro SWAP nn
+;;; 8 B  19c
+        ldx VARa-1+nn
+        pla
+        sta VARa-1+nn
+        txa
+        pha
+        pla
+.endmacro
+
+;;; (+ 8 8 8 5 64 4) = 97 B
+swap2:  
+;;; 8 B
+        tsx
+        stx savex
+        ;; discount call here
+        pla
+        pla
+        jmp sw2
+swap4:  
+;;; 8 B
+        tsx
+        stx savex
+        ;; discount call here
+        pla
+        pla
+        jmp sw4
+swap6:  
+;;; 8 B
+        tsx
+        stx savex
+        ;; discount call here
+        pla
+        pla
+        jmp sw6
+swap8:  
+;;; 5 B
+        tsx
+        stx savex
+        ;; discount call here
+        pla
+        pla
+
+;;; (* 8 8) = 64
+        SWAP 8
+        SWAP 7
+sw6:    
+        SWAP 6
+        SWAP 5
+sw4:    
+        SWAP 4
+        SWAP 3
+sw2:    
+        SWAP 2
+        SWAP 1
+
+;;; 4 B
+        ldx savex
+        txs
+        rts
+
+.else ; !CALLSWAP8
+
+swapY:  
+;;; 24 B (smaller and faster!)
+        tsx
+        stx savex
+        ;; skip call here
+        pla
+        pla
+:       
+        ;; swap byte
+        ;; TODO: use ,x to do zero, save bytes
+        ldx VARa-1,y
+        pla
+        sta VARa-1,y
+        txa
+        pha
+        ;; step up
+        pla                     ; s-- !
+        dey
+        bne :-
+
+        ;; restore stack pointer!
+        ldx savex
+        txs
+
+        rts
+.endif ; !CALLSWAP8
+
+
+
+
+
+
+
+.ifdef RESTORY
+
+restoreY:
+;putc '?'
+;;; RESTORE!
+;;; 13 B
+        sta savea
+        pla
+        tay
+;        ldy #8
+;;; 13 c ok, it's faster...
+;;; could generate a long sequence and jump middle
+;;; wquld be 6c faster/byte! (8 => 42c!)
+:       
+        pla
+        sta VARa-1,y
+        dey
+        bne :-
+
+        lda savea
+        rts
+
+.else
+
+;;; (+ 27 4 4 4) = 39 B
+;;; 10% faster than cc65!
+
+.macro PLOP nn
+        pla
+        sta VARa-1+nn
+.endmacro
+
+
+
+restore8:       
+;;; (+ 1 1 1 (* 8 (+ 1 2))) = 27 B
+        tay
+        PLOP 8
+        PLOP 7
+rest6:  
+        PLOP 6
+        PLOP 5
+rest4:  
+        PLOP 4
+        PLOP 3
+rest2:  
+        PLOP 2
+        PLOP 1
+        tya
+        rts
+
+restore6:       
+;;; +4 B
+        tay
+        jmp rest6
+restore4:       
+;;; +4 B
+        tay
+        jmp rest4
+restore2:       
+;;; loop (+ 3 4 2 3 (* 2 (+ 4 4 2 3))) = 38c
+
+;;; (+ 2 3 2 (* 2 (+ 4 3))) = 21c (overhead 3c)
+;;; 4 B (+ 3c)
+        tay
+        jmp rest2
+
+;;; +5 B  saves  3c => not worth it!
+;;; (+ 2 2 (* 2 (+ 4 3))) = 18c
+;;; 9 B = 18c
+.ifnblank
+        tay
+        PLOP 2
+        PLOP 1
+        tya
+        rts
+.endif
+
+.endif ; !RESTORY
+
+
+.endif ; JSK_CALLING
+
+.endif ; RECURSION
+
+
 FUNC _runtimeend
 
 
@@ -1271,10 +1595,11 @@ FUNC _bnfinterpstart
 ;
 CHECKSTACK=1
 
-;;; Zeropage vars should save many byes!
+;;; Zeropage vars should save many bytes!
 ;
 ZPVARS=1
 
+;;; TODO: not working yet
 ;;; Minimal set of rules (+ LIBRARY)
 ;MINIMAL=1
 
@@ -1284,8 +1609,10 @@ ZPVARS=1
 ;
 OPTRULES=1
 
-        ;; 
+;; 
 
+; Remove to not support else; leaner IF... lol
+;
 ELSE=1
 
 ;;; Byte optimized rules
@@ -1369,13 +1696,6 @@ PRINTINPUT=1
 
 ;;; more compact printing of source when compiling
 ;UPDATENOSPACE=1
-
-
-
-;;; TODO: sim65 crashes!!!!! when PRINTASM, it happends inside
-;;;    jsr asmprintsrc which loops giving ";  m222222222222"?????
-
-;;;                                       W   T   F?
 
 
 ;;; TODO: make it a runtime flag, if asm is included?
@@ -4332,16 +4652,14 @@ FUNC _memoryrulesend
       .byte "]"
 
 
-;;; TODO: disturbes parseing, maybe %{ ?
-
-;
-JSK_CALLING=1
 .ifndef JSK_CALLING
 
-
+;;; TODO: REMOVE!
+;;;    this is just a prototype experiement
+;;;    to get to RECURSIVE... keep for now
 
         ;; Function call!!!
-;;; TODO: for 0 args this still pushes, lol
+        ;; TODO: for 0 args this still pushes, lol
         .byte "|%U[#]("
         .byte _W
       .byte "[;"
@@ -6622,14 +6940,35 @@ VARa= vars+('a'-'A')*2
 ;
 PRELUDE=1
 ;
+
+
+
+.ifdef OPTJSK_CALLING
+;POSTLUDE=1
+.else
 POSTLUDE=1
+.endif
+
+
+
 
 .ifdef PRELUDE
       .byte "["
-;REVERSE=1
-.ifndef REVERSE
-;;; TODO: new one use jsr
 
+.ifdef JSK_CALLING
+
+.ifdef CALLSWAP8
+
+        jsr swap8
+        
+.else ; !CALLSWAP8
+
+.ifblank
+;;;  // TEST
+        ldy #8
+        jsr swapY
+        
+.else
 ;;; 21 B (smaller and faster!)
         tsx
         stx savex
@@ -6652,8 +6991,39 @@ POSTLUDE=1
         ;; restore stack pointer!
         ldx savex
         txs
+.endif ; blank
+
+.endif ; CALLSWAP8
+
+
+;;; This creates a deferred call to cleanup
+;;; after the function does an RTS
+
+.ifdef OPTJSK_CALLING
+        ;; defer: restore(8)
+;putc '!'
+.ifblank
+;;; 6 B
+;;; TODO: code that generates specific caLL
+        lda #>(restore8-1)
+        pha
+        lda #<(restore8-1)
+        pha
 .else
-putc 'S'
+;;; 9 B
+        lda #8
+        pha
+        lda #>(restoreY-1)
+        pha
+        lda #<(restoreY-1)
+        pha
+.endif
+
+.endif ; OPTJSK_CALLING        
+
+
+.else ; !JSK_CALLING
+
 ;;; 28 B (smaller and faster!)
 
         ;; swap stack w registers!
@@ -6683,7 +7053,7 @@ putc 'S'
         ;; restore stack pointer!
         ldx savex
         txs
-.endif ; !REVERSE
+.endif ; !JSK_CALLING
       .byte "]"
 .endif ; PRELUDE
 
@@ -6692,6 +7062,7 @@ putc 'S'
       .byte "["
 
 .ifdef POSTLUDE
+
         ;; postlude
         ;; restore register bytes from stack
         ;; (doesn't care order)
@@ -6701,7 +7072,7 @@ putc 'S'
 ;;; TODO: maybe no need saving?
         stx savey
 
-.ifndef REVERSE
+.ifdef JSK_CALLING
 ;;; RESTORE!
 ;;; 9 B
         ldy #8
@@ -6762,7 +7133,7 @@ putc 'S'
         ldx savex
         txs
 .endif ; DOSWAP
-.endif ; !REVERSE
+.endif ; !JSK_CALLING
 
 ;;; NOBODY else can currently return
 ;;;   we just need to keep AX safe...
@@ -8380,6 +8751,9 @@ parsevarcont:
 
         .byte 0
 
+
+
+
 FUNC _stmtrulesend
 
 
@@ -8688,6 +9062,7 @@ endrules:
 
 ;;; END rules
 ;;; ========================================
+
 
 FUNC _rulesend
 
@@ -10806,15 +11181,13 @@ CANT=1
         .byte "}",10
         .byte "",10
         .byte "word main() {",10
+        .byte "  return F(22, 0, 1, 65535);",10
         .byte "}",10
         .byte 0
 .else ; CANT
-;;; cc02 cannot handle arguments just yet
 ;        .byte "word F(word a, word b, word c, word d) {",10
-;;; TDOO:
 
-;;; 598475 compile
-;;; 642428 run (- 642428 598475) = 43953
+
 .ifdef NOTHING
         .byte "word main() {",10
         .byte "}",10
@@ -10848,8 +11221,8 @@ CANT=1
 .ifdef P4PR
         .byte "  putchar('>'); P();",10
 .endif
-        .byte "  if (a) r= F(a-1, b+1, d*2, c/2);",10
-        .byte "  else r= a+b+c+d;",10
+        .byte "  if (a) return F(a-1, b+1, d*2, c/2);",10
+        .byte "  else return a+b+c+d;",10
 ;        .byte "  r= a+b+c+d;",10
 .ifdef P4PR
         .byte "  putchar('<'); P();",10
@@ -10858,53 +11231,21 @@ CANT=1
 ;;; need to run postlude...
 ;        .byte "  return r;",10
 ;;; set AX
-        .byte "  r;",10
+;        .byte "  r;",10
 
         .byte "}",10
         .byte "word main() {",10
-        .byte "//#x1234  #x5678   #x1122 #x3344",10
-        .byte "  a=4660;b=22136;c=4386;d=13124;e=0;",10
 .ifdef P4PR
         .byte "  putchar('+'); P();",10
 .endif
-;        .byte "  return 4711;",10
         .byte "i=1000;while(i--){",10
 ;.byte "i=1;while(i--){",10
-        .byte "  r= F(22, 0, 1, 65535);",10
+;        .byte "  r= F(22, 0, 1, 65535);",10
+;;; (/ 256 (+ 8 2 1 3)) 
+        .byte "  r= F(18, 0, 1, 65535);",10
 ;        .byte "  r= F(0, 9, 1, 65535);",10
 ;;; I think value comes out wrong???
 .byte "}",10
-
-;;; 1078241
-;;; 12507300 (/ (- 12507300 1078241) 1000 23) = 496
-;;; using JSK_CALLING! 267 => 279 bytes?
-;;; uses extra 6c (+6B jumps) but saves
-
-
-;;; 22 max: (* 22 (+ 8 2)) = 220 !
-;        .byte "  r= F(22, 0, 1, 65535);",10
-;        .byte "  r= F(3, 0, 1, 65535);",10
-
-;;;  43953 c overhead/start
-;;; 692453 compile
-;;; 732569 (- 732569 692453 43953) 1x
-;;; 
-
-;;;    0                 1           10      100
-;;; 791254 compile    820571       820695    820942
-;;; 834224 run 0      860660       865860   1424480
-;;; 
-;;; (/ (- 1424480 820942) 100) = 6035c !!!!????
-
-;;;  1065057
-;;;  1695899 (/ (- 1695899 1065057) 1000) = 630
-;;; 13821630 (/ (- 13821630 1065057) 1000 23) = 554!!!
-;;; 
-;;; 554! cycles, so ok (/ 554 493.0) =
-;;;       12.4% slower than cc65
-;;;  1072631
-;;; 13821624 (/ (- 13821624 1072631) 1000 23) = 554
-
 
 
 .ifdef P4PR
