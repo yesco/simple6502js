b8d36131cffb53e9a195ee2870681bdf7119eec4
Jonas S Karlsson <jsk@yesco.org>
2025-12-30 08:48:34 +0700

    soundsort: added an almost working quicksort - maybe off by 1? Made a summary score board w timing in cs

--- 65lisp/Input/soundsort.c
+++ 65lisp/Input/soundsort.c
@@ -43,12 +43,15 @@ void show(char r, char color) {
   while(c<40) p[++c]= 0;
 }
 
-#define DBLE 10;
+#define DBLE 10
+
+char *lastname= NULL;
 
 void printname(char* s) {
   char c= (40-strlen(s))/2;
   char *p= TEXTSCREEN + 25*40;
 
+  lastname= s;
   memset(p, ' ', 3*40);
 
   p+= 40+c;
@@ -60,32 +63,6 @@ void printname(char* s) {
 
   *p= DBLE;
   strcpy(p+1, s);
-}
-
-void selectionsort() {
-  char r, i, v, vv, at, t;
-
-  printname("SelectionSort");
-
-  for(r= 0; r<HIRESROWS-1; ++r) {
-    // find smallest
-    at= r; v= get(r, 1);
-    for(i= r+1; i<HIRESROWS; ++i) {
-      vv= get(i, 1);
-      if (vv < v) {
-        at= i; v= vv;
-      }
-      get(i, 7);
-    }
-
-    // swap w smallest
-    t= arr[r];
-    arr[r]= v;
-    arr[at]= t;
-
-    show(at, 4); // indicate swapped
-    show(r, 7);  // reset color
-  }
 }
 
 void insertsort() {
@@ -122,6 +99,133 @@ void insertsort() {
   }
 }
 
+void selectionsort() {
+  char r, i, v, vv, at, t;
+
+  printname("SelectionSort");
+
+  for(r= 0; r<HIRESROWS-1; ++r) {
+    // find smallest
+    at= r; v= get(r, 1);
+    for(i= r+1; i<HIRESROWS; ++i) {
+      vv= get(i, 1);
+      if (vv < v) {
+        at= i; v= vv;
+      }
+      get(i, 7);
+    }
+
+    // swap w smallest
+    t= arr[r];
+    arr[r]= v;
+    arr[at]= t;
+
+    show(at, 4); // indicate swapped
+    show(r, 7);  // reset color
+  }
+}
+
+void bubblesort() {
+  char r, i, v, vv;
+
+  printname("BubbleSort");
+
+  i= 1;
+  while(i) {
+    i= 0;
+    for(r= 0; r<HIRESROWS-1; ++r) {
+      v = get(r,   1);
+      vv= get(r+1, 1);
+      if (v > vv) {
+        ++i;
+        arr[r]  = vv;
+        arr[r+1]= v;
+        show(r,  7);
+        show(r+1, 7);
+      } else {
+        get(r,   7);
+        get(r+1, 7);
+      }
+    }
+  }
+}
+
+void qs(char a, char b) {
+  // only one - nothing to sort
+  if (a>=b) return;
+
+  // swap if 2 in wrong order
+  if (a+1==b) {
+    char v= get(a, 1);
+    char vv= get(b, 1);
+    if (vv < v) {
+      arr[a]= vv;
+      arr[b]= v;
+
+      show(a, 7);
+      show(b, 7);
+    } else {
+      get(a, 7);
+      get(b, 7);
+    }
+    return;
+
+  } else {
+
+    // else divide by middle pivot 
+    char pi = (a+b)/2;
+    char pv = get(pi, 1);
+    char oa= a, ob= b;
+    char v, vv;
+
+    while(a<b) {
+      // move up lower boundary if < 
+      --a;
+      do {
+        ++a;
+        v= get(a, 1);
+      } while(a<b && v<pv);
+      
+      // move down upper boundary if >=
+      ++b;
+      do {
+        --b;
+        vv= get(b, 1);
+      } while(a<b && vv>=pv);
+
+      if (a<b) {
+        // two elt out of place: swap
+        arr[b]= v;
+        arr[a]= vv;
+      
+        show(a, 4);
+        show(b, 4);
+      }
+    }
+    pi= a;
+
+    // clear colors
+    a= oa; b= ob;
+    while(a<b) get(a++, 7);
+
+    // sort the parts
+    qs(oa, pi);
+    qs(pi+1, ob);
+  }
+}
+
+void quicksort() {
+  printname("BubbleSort");
+
+  qs(0, HIRESROWS-1);
+}
+
+#define METHODS 4
+
+word csecs[METHODS+1]= {0};
+char* name[METHODS+1]= {0};
+
+
 void main() {
   char r, m;
   char *p= HIRESSCREEN;
@@ -136,7 +240,7 @@ void main() {
   }
 
   // run through different sorts
-  for(m=1; m<=2; ++m) {
+  for(m=1; m<=METHODS; ++m) {
 
     play(1, 0, 0, 10);
     sfx(SILENCE);
@@ -150,14 +254,30 @@ void main() {
       show(r, 7);
     }
 
+    // reset timer to avoid wraparound
+    // (/ 65535 100 60) = 10 minutes (665 s)
+    *(int*)0x276= 0xffff;
+    csecs[m]= time();
+
     switch(m) {
 
-    case 1: insertsort(); break;
+    case 1: quicksort(); break;
     case 2: selectionsort(); break;
-//    case 3: quicksort(); break;
+    case 3: insertsort(); break;
+    case 4: bubblesort(); break;
 
-// TODO: doesn't work?
     }
+
+    csecs[m]-= time();
+    name[m]= lastname;
+
+
+    // print timing
+    sprintf(TEXTSCREEN+ 40*26 + 32, "%c%d", 3, csecs[m]);
+    sprintf(TEXTSCREEN+ 40*27 + 32, "%c%d", 3, csecs[m]);
+
+
+    // TODO: test is sorted - lol!
 
     // show result
     for(r= 0; r<HIRESROWS; ++r) {
@@ -165,12 +285,25 @@ void main() {
       show(r, 2);
     }
 
+
     sfx(SILENCE);
-    
+
     wait(150);
   }
  
+  // show summary result
+  text();
+  clrscr();
 
+  putchar('\n');
+  for(m=1; m<=METHODS; ++m) {
+    sprintf(TEXTSCREEN+ 40*(8+m*2) + 8, "%c%5d  %s\n",
+            DBLE, csecs[m], name[m]);
+    sprintf(TEXTSCREEN+ 40*(9+m*2) + 8, "%c%5d  %s\n",
+            DBLE, csecs[m], name[m]);
+  }
+
+  // don't mess up the display
 halt:
   goto halt;
 }
