3572cffb6d29058c2f33dd2a2c31f6de8b20e124
Jonas S Karlsson <jsk@yesco.org>
2026-01-18 22:57:29 +0700

    info: update colors

--- 65lisp/parse.c
+++ 65lisp/parse.c
@@ -352,7 +352,7 @@ void info() {
 
 
   printf
-    ("\n\x97\x84- CC02  MeteoriC-6502-compiler -  \x90\n"
+    ("\n\x94\x87- CC02  MeteoriC-6502-compiler -  \x90\n"
 // TAP-file
      "C %u main %u (loader) TEXT: ?%u\n"
      "  (disasm)   %6u - ^Q disasm code\n"
