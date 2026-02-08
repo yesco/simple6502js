492c51fe6612548e62f2cc8a61955f0416415a86
Jonas S Karlsson <jsk@yesco.org>
2026-02-09 03:27:45 +0700

    add debug-array.c

---
+++ 65lisp/Input/debug-array.c
@@ -0,0 +1,11 @@
+word a,b,c;
+char sa[]="foo";
+char sb[]="foo";
+char sc[]={'f','o','o',0};
+word main(){
+  puth(&a); puth(&b); puth(&c); putchar('\\n');
+  puth(&sa); puts(sa);
+  puth(&sb); puts(sb);
+  puth(&sc); puts(sc);
+  return 4711;
+}
